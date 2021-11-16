366f0447fdf4df01ed6df88ddfa99ebdb5048c1f
sjoongh <az45687@naver.com>
2021-11-16 14:54:37 +0900

    search hashtag fix

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,125 +1,230 @@
 <template>
     <form @submit.prevent="onSubmit">
-        <div id="app"
+        <!-- <div id="app"
         v-on:keyup.down="selectValue('down')"
         v-on:keyup.up="selectValue('up')">
         <input type="text" v-model="inputValue" placeholder="검색어를 입력하세요" autofocus @keyup="onKeyup">
         <button type="reset" @click="onClickResetBtn" v-show="inputValue.length" class="btn-reset"></button>
-        </div>
+        </div> -->
+        <div class="wrap">
+  <div id="app"
+       v-on:keyup.down="selectValue('down')"
+       v-on:keyup.up="selectValue('up')">
+    <div class="search">
+      <input class="s" placeholder="'#'을 써보세요"
+             v-on:input="searchQuery=$event.target.value"
+             v-model="id">
+             <!-- v-model="searchQuery" autofocus @keyup="onKeyup" -->
+      <!-- <button type="reset" @click="onClickResetBtn" v-show="searchQuery.length" class="btn-reset"></button> -->
+      <!-- link추가와 최근검색어 기능 넣을지? -->
+      <!-- input값 감싸서 상세검색페이지에 보내면 hashtag값 선택 -->
+      <!-- vuex??사용해야되나 -->
+      <!-- 키보드로도 선택이 가능한데 필요한가??.. -->
+      <ul class="r" tabindex="0"
+          v-bind:class="{ show: isActive }"
+          >
+        <li tabindex="-1"
+            v-for="(el, index) in filterList" :key="index"
+            v-on:click="changeValue(el.name)"
+            v-on:keyup.enter="selectValue('enter', el.name)">
+          <span>{{ el.name }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
     </form>
 </template>
 
 <script>
-export default {
-    names: [
+var names = [
+  { name: '#송파구', id:1 },
+  { name: '#송파구 잠실', id:2 },
+  { name: '#잠실 카트장', id:3 },
+  { name: '#잠실 놀거리', id:4 },
+  { name: '#잠실 카페' },
+  { name: '#송리단길' },
   { name: '#서울시' },
-  { name: '#서리' },
-  { name: '#서규동' },
-  { name: '#규리서비' },
-  { name: '#송파구' },
-  { name: '#잠실' },
-  { name: '#잠실 카트장' },
-  { name: '#카페 맛집 잠실' },
-  { name: '#자리잠' },
-  { name: '#시울서' },
-  { name: '#just test' },
-],
-    props:['value'],
-    data(){
-        return{
-            inputValue: this.value
-        }
-    },
-    watch:{
-        value(newVal, oldVal){
-            this.inputValue = newVal
-        }
-    },
-    methods:{
-        onSubmit(){
-            this.$emit('@submit',this.inputValue.trim())
-        },
-        onClickResetBtn(){
-            this.inputValue = ''
-            this.$emit('@reset')
-        },
-        onKeyup(){
-            if(this.inputValue.length === 0){
-                this.$emit('@reset')
-            }
-        }
+  { name: '#서리단길' },
+  { name: '#석가래' },
+  { name: '#잠자리' },
+  { name: '#서잠' }
+]
+
+export default {
+  data () {
+    return {
+      isActive: false,
+      searchQuery: '',
+      names: names,
     }
-}
-var app = new Vue({
-    el: '#app',
-    data: {
-        isActive: false,
-        searchQuery: '',
-        names: names,
   },
+  // watch:{
+  //       value(newVal, oldVal){
+  //         console.log(newVal)
+  //           this.searchQuery = newVal
+  //       }
+  //     },
   methods: {
-    changeValue(str) {
-      this.isActive = false;
-      document.querySelector('.s').value = str;
+    onSubmit(event){
+      // vuex로 보내거나
+      // axios로 hashname db로 보내줌
+      // axios.post()
+      console.log()
+      console.log(this.id)
+      console.log(this.searchQuery.trim())
+      this.$emit('@submit',this.searchQuery.trim())
+      },
+    //     onClickResetBtn(){
+    //         this.searchQuery = ''
+    //         this.$emit('@reset')
+    //     },
+    //     onKeyup(){
+    //         if(this.searchQuery.length === 0){
+    //             this.$emit('@reset')
+    //         }
+    //     },
+    changeValue (str) {
+      console.log(`change value: ${str}`)
+      this.isActive = false
+      this.searchQuery = ''
+      document.querySelector('.s').value = str
     },
-    selectValue(keycode, str) {
+    selectValue (keycode, str) {
       if (this.isActive === true) {
-        const hasClass = document.querySelector('.r').classList.contains('key');
+        const hasClass = document.querySelector('.r').classList.contains('key')
         if (keycode === 'down') {
           if (!hasClass) {
-            const thisEl = document.querySelectorAll('.r li')[0];
-            document.querySelector('.r').classList.add('key');
-            thisEl.classList.add('sel');
-            thisEl.focus();
+            const thisEl = document.querySelectorAll('.r li')[0]
+            document.querySelector('.r').classList.add('key')
+            thisEl.classList.add('sel')
+            thisEl.focus()
           } else {
-            const lastEl = document.querySelector('.r li:last-child');
-            const thisEl = document.querySelector('.r li.sel');
-            const nextEl = thisEl.nextElementSibling;
+            const lastEl = document.querySelector('.r li:last-child')
+            const thisEl = document.querySelector('.r li.sel')
+            const nextEl = thisEl.nextElementSibling
             if (!lastEl.classList.contains('sel')) {
-              thisEl.classList.remove('sel');
-              nextEl.classList.add('sel');
-              nextEl.focus();
+              thisEl.classList.remove('sel')
+              nextEl.classList.add('sel')
+              nextEl.focus()
             }
           }
         }
         if (keycode === 'up' && hasClass) {
-          const firstEl = document.querySelectorAll('.r li')[0];
-          const thisEl = document.querySelector('.r li.sel');
-          const prevEl = thisEl.previousElementSibling;
+          const firstEl = document.querySelectorAll('.r li')[0]
+          const thisEl = document.querySelector('.r li.sel')
+          const prevEl = thisEl.previousElementSibling
           if (!firstEl.classList.contains('sel')) {
-            thisEl.classList.remove('sel');
-            prevEl.classList.add('sel');
-            prevEl.focus();
+            thisEl.classList.remove('sel')
+            prevEl.classList.add('sel')
+            prevEl.focus()
           } else {
-            document.querySelector('.s').focus();
+            document.querySelector('.s').focus()
           }
         }
         if (keycode === 'enter' && hasClass) {
-          this.changeValue(str);
+          this.changeValue(str)
         }
       }
-    },
-    removeValue() {
-      if (document.querySelector('.r').classList.contains('key')) {
-        document.querySelector('.r').classList.remove('key');
-        document.querySelector('.r li.sel').classList.remove('sel');
-      }
-    },
+    }
   },
   computed: {
-    filterList() {
-      const str = this.searchQuery;
-      const reg = /[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9|#|\s]/.test(str);
-      console.log(`typing value: ${str}`);
+    filterList () {
+      const str = this.searchQuery
+      const reg = /[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9|\s]/.test(str)
+      console.log(`typing value: ${str}`)
       if (reg === false && str !== '' && str !== ' ') {
-        this.isActive = true;
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        this.isActive = true
+        console.log(this.isActive)
+        console.log(2)
         return this.names.filter((el) => {
-          return el.name.match(str);
-        });
+          return el.name.match(str)
+        })
       } else {
-        this.isActive = false;
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        this.isActive = false
       }
-    },
-  },
-});
+    }
+  }
+}
 </script>
+
+<style scoped>
+html, body {
+  height: 100%;
+}
+
+body {
+  background-color: #ddd;
+  font-size: 14px;
+  color: #333;
+}
+
+strong {
+  font-weight: bold;
+}
+
+.wrap {
+  display: table;
+  padding: 200px 20px;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+}
+
+#app {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+}
+#app .search {
+  position: relative;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 600px;
+}
+#app .search .s {
+  padding: 10px 20px;
+  width: 100%;
+  max-width: 600px;
+  height: 60px;
+  box-sizing: border-box;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
+  border: 1px solid #888;
+  font-size: 16px;
+}
+#app .search .r {
+  display: none;
+  position: absolute;
+  left: 0;
+  top: 60px;
+  width: 100%;
+  height: 156px;
+  overflow-y: auto;
+}
+#app .search .r.show {
+  display: block;
+}
+#app .search .r li {
+  margin-top: -1px;
+  padding: 0 20px;
+  width: 100%;
+  height: 40px;
+  background-color: #fff;
+  box-sizing: border-box;
+  border: 1px solid #888;
+  outline: none;
+  font-size: 16px;
+  line-height: 40px;
+  cursor: pointer;
+}
+#app .search .r li:hover, #app .search .r li.sel {
+  background-color: #f2f2f2;
+}
+#app .search p {
+  padding: 10px 0;
+  text-align: right;
+  font-size: 12px;
+}
+</style>
